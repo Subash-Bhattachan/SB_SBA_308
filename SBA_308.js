126865ad84121b2c8de5dd904a2178c19d0b30b1
Subash-Bhattachan <73665341+Subash-Bhattachan@users.noreply.github.com>
2024-10-09 02:35:48 -0400

    The logic is changed

--- SBA_308.js
+++ SBA_308.js
@@ -76,146 +76,83 @@ const learnerSubmissions = [
   },
 ];
 
-// Gathering data
-// Making functions
-function validateAssignmentGroup (courseInfo, assignmentGroup) {
-    if (courseInfo.id !== assignmentGroup.course_id)
-    {console.log("The input is invalid.")}
-    else 
-    {return courseInfo.id === assignmentGroup.course_id;}
-}
-
-
-function validateSubmission (learnerSubmission, assignments) {
-    if (assignments.points_possible !== 0 || 
-        learnerSubmission.score !== 0) {
-            return false;}
-    else {
-        return true;
-    }
-
-}
-
-function calculateWeightedAverage (learnerSubmission, assignments) {
-    return (learnerSubmission.score / assignments.points_possible ) * 100;
-}
-
-
-
 function getLearnerData(course, ag, submissions) {
-  
-  const result = [];
- const studentData= {};
-  
+  const result = []; // for the final array output
+  const studentData = {}; // object to hold all the details regarding the student
+  let total_score = 0;
+  let total_possible_score = 0;
+  let avg = 0;
+
+  // first running the outer for loop to run through the condition that the course ID is defined everytime one runs the application
   try {
     if (course.id === undefined) {
-        console.log ("Invalid input.");
+      console.log("Invalid input.");
     } else {
-
-        for (let i = 0; i < submissions.length; i++) {
-          
-          if (ag.course_id !== course.id) {
-            console.log("Mismatched!");
-          }
-          else {
+      for (let i = 0; i < submissions.length; i++) {
+        if (ag.course_id !== course.id) {
+          console.log("Mismatched!");
+        } else {
           break;
+        }
+      }
+
+      // Running the second for loop to run through the assignments length to extract the required result and hence compare on the basis of that
+      for (let j = 0; j < ag.assignments.length; j++) {
+        //console.log("so far ok-1st");
+
+        // now comparing the due dates and submission dates
+
+        const submittedDate = new Date(submissions[j].submission.submitted_at);
+        const dueDate = new Date(ag.assignments[j].due_at);
+        const currentDate = new Date();
+
+        //  console.log(submittedDate);
+        //  console.log(dueDate);
+        //  console.log("6!!")
+
+        let pointsPossible = ag.assignments[j].points_possible;
+        let submissionScore = submissions[j].submission.score;
+
+        if (dueDate < currentDate) {
+          learnerAssignId = submissions[j].assignment_id;
+          assignmentId = ag.assignments[j].id;
+          //studentData[j].avg = avg;
+          studentData["id"] = submissions[j].learner_id;
+          studentData["avg"] = submissionScore / pointsPossible;
+          //console.log("good " + avg)
+          // console.log(pointsPossible)
+          // console.log(submissionScore)
+        }
+
+        if (learnerAssignId === assignmentId) {
+          if (submittedDate > dueDate) {
+            //take off 10% of the score
+
+            studentData["id"] = submissions[j].learner_id;
+            studentData["avg"] = (submissionScore / pointsPossible) * 0.9;
+            console.log(submissionScore / pointsPossible);
+            total_score += submissionScore * 0.9;
+            total_possible_score += pointsPossible;
+          } else {
+            total_score += submissionScore;
+            total_possible_score += pointsPossible;
           }
-
         }
-        
-
-        //
-          //const studentData = {}; // creating empty object for student data
-          // submissions.learner_id = id; // pushing objects of interest
-          // studentData.avg = avg;
-          // studentData.assignment_id = number;
-          
-          let total_score = 0;
-          let total_possible_score = 0;
-          let avg = 0;
-              
-
-         for (let j = 0; j < ag.assignments.length; j++) {
-          //for (const [key, value] of Object.entries(studentData)) {
-           console.log("so far ok-1st");
-
-
-           // now comparing the due dates and submission dates
-
-           const submittedDate = new Date(submissions[j].submission.submitted_at);
-           const dueDate = new Date(ag.assignments[j].due_at);
-           const currentDate = new Date();
-          
-          
-          
-           console.log(submittedDate);
-           console.log(dueDate);
-           console.log("6!!")
-
-
-           
-           let pointsPossible = ag.assignments[j].points_possible;
-           let submissionScore = submissions[j].score;
-           let avg = submissionScore / pointsPossible;
-
-
-          
-
-                if (dueDate < currentDate) {
-
-                  let learnerAssignId = submissions[j].assignment_id;
-                  let assignmentId = ag.assignments[j].id;
-                  //studentData[j][0] = avg;
-                  console.log(avg)
-
-                }
-
-                //if (ag.assignments[j].id === submissions[j].assignment_id) {
-                
-                if (submittedDate > dueDate) {
-                //print out the total average
-                //let submissionScore = submissions[j].score;
-                studentData[j][0] = avg * 0.9
-                console.log(submissionScore);
-                  //return avg;
-                total_score += submissionScore;
-                total_possible_score += pointsPossible;
-                console.log("#####")
-                  }
-                else {
-                      total_score += submissionScore;
-                      total_possible_score += pointsPossible;
-                    }
-            
-
-         
-
-        }
-
-        }
-     
-   } catch (error) {
-     console.log(error);
-     //studentData["avg"] = total_score / total_possible_score;
-    
-    
-    
-     
+      }
+    }
+  } catch (error) {
+    console.log(error);
   }
+  studentData["avg"] = (total_score / total_possible_score).toFixed(2);
   //console.log("error message: There is an error.")
-  result.push(studentData)
+  result.push(studentData);
   return result;
 }
-
-  // CourseInfo
-
-  // AssignmentGroup
-
-  // LearnerSubmissions
-  //result.push(studentData)
-  
-
 
 const result = getLearnerData(courseInfo, assignmentGroup, learnerSubmissions);
 
 console.log(result);
+
+// All the outputs are not printing yet in my case, through I tried a lot.
+// I feel there is something still be figured out in if and else statement as the
+// rest of the statements are not being tracked according to the date of submissions made.
